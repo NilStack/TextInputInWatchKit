19836e8960e7821fbbde4f5e589c16e8a67b5ce3
Peng Guo <guoleii@gmail.com>
2015-03-17 01:13:01 +0800

    first commit

---
+++ TextInputInWatchKit WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  TextInputInWatchKit WatchKit Extension
+//
+//  Created by Peng on 3/16/15.
+//  Copyright (c) 2015 Peng. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ TextInputInWatchKit WatchKit Extension/InterfaceController.m
@@ -0,0 +1,38 @@
+//
+//  InterfaceController.m
+//  TextInputInWatchKit WatchKit Extension
+//
+//  Created by Peng on 3/16/15.
+//  Copyright (c) 2015 Peng. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (void)awakeWithContext:(id)context {
+    [super awakeWithContext:context];
+
+    // Configure interface objects here.
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    [super willActivate];
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    [super didDeactivate];
+}
+
+@end
+
+
+
